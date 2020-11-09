f27123d85c50933be2b517174698f58084287945
hooneec <hooneec1117@gmail.com>
2020-11-10 08:46:11 +0900

    2020/11/10(TUE) 08:46

---
+++ 03_FT_PRINTF/ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeunjeon <jeunjeon@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/09 19:53:54 by jeunjeon          #+#    #+#             */
+/*   Updated: 2020/11/09 19:55:34 by jeunjeon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+int		ft_printf(const char a, ...);
+
+#endif
