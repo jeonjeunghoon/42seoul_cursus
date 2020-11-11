cb8d090b00a94e92a3f2062a58ee43603799cce2
hooneec <hooneec1117@gmail.com>
2020-11-11 17:57:48 +0900

    2020/11/11(WED) 17:57

--- 03_FT_PRINTF/ft_printf.c
+++ 03_FT_PRINTF/ft_printf.c
@@ -6,13 +6,43 @@
 /*   By: jeunjeon <jeunjeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/09 19:52:10 by jeunjeon          #+#    #+#             */
-/*   Updated: 2020/11/09 19:53:30 by jeunjeon         ###   ########.fr       */
+/*   Updated: 2020/11/11 17:40:45 by jeunjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <stdio.h>
 
-int		ft_printf(const char s, ...)
+int			format_specifier(char *format, va_list ap)
 {
 	
+}
+
+int			ft_printf(const char *format, ...)
+{
+	va_list	ap;
+	int		res;
+
+	va_start(ap, format);
+	res = 0;
+	while (*format)
+	{
+		if (*format == '%')
+			res = format_specifier(format, ap);
+		else 
+		{
+			write(1, format, 1);
+			res++;
+			format++;
+		}
+	}
+	va_end(ap);
+	return (res);
+}
+
+int main()
+{
+	printf("out: %d\n", printf("hello\n"));
+	printf("OUT: %d\n", ft_printf("HELLO\n"));
+	// printf("%c", 'a');
 }
--- 03_FT_PRINTF/ft_printf.h
+++ 03_FT_PRINTF/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: jeunjeon <jeunjeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/09 19:53:54 by jeunjeon          #+#    #+#             */
-/*   Updated: 2020/11/09 19:55:34 by jeunjeon         ###   ########.fr       */
+/*   Updated: 2020/11/11 16:20:41 by jeunjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@
 # include <stdlib.h>
 # include <stdarg.h>
 
-int		ft_printf(const char a, ...);
+int		ft_printf(const char *format, ...);
 
 #endif
