98893745c163b1f959cb0085cdefe4d040214e02
jeonjeunghoon <jeonjeunghoon@gmail.com>
2021-07-16 17:59:09 +0900

    Add dir: 08_PIPEX

---
+++ 08_PIPEX/inc/pipe_x.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipe_x.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeunjeon <jeunjeon@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/16 17:50:08 by jeunjeon          #+#    #+#             */
+/*   Updated: 2021/07/16 17:58:18 by jeunjeon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPE_X_H
+# define PIPE_X_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <stdio.h>
+# include <string.h>
+
+typedef struct	s_arg
+{
+	char	*infile;
+	char	*outfile;
+	char	*cmd1;
+	char	*cmd2;
+} t_arg;
+
+
+#endif
