145230cc9769592a44ab2cc652aa81b6cb885a7f
jeonjeunghoon <jeonjeunghoon@gmail.com>
2022-01-05 16:25:01 +0900

    Add: get_user_input

--- 10_MINISHELL/srcs/ft_prompt.c
+++ 10_MINISHELL/srcs/ft_prompt.c
@@ -6,11 +6,21 @@
 /*   By: jeunjeon <jeunjeon@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 10:42:58 by jeunjeon          #+#    #+#             */
-/*   Updated: 2022/01/05 16:22:25 by jeunjeon         ###   ########.fr       */
+/*   Updated: 2022/01/05 16:24:34 by jeunjeon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
+
+char	*get_user_input(t_mini *mini)
+{
+	char	*buff;
+
+	buff = readline(mini->prompt);
+	free(mini->prompt);
+	mini->prompt = NULL;
+	return (buff);
+}
 
 int	ft_prompt(t_mini *mini)
 {
@@ -20,11 +30,9 @@ int	ft_prompt(t_mini *mini)
 	if (load_prompt(mini) == ERROR)
 		return (ERROR);
 	user_input = NULL;
-	user_input = readline(mini->prompt);
+	user_input = get_user_input(mini);
 	if (user_input == NULL)
 		return (ERROR);
-	free(mini->prompt);
-	mini->prompt = NULL;
 	mini->splitted_input = ft_split(user_input, ' ');
 	if ((ft_command(mini, user_input, mini->splitted_input)) != ENTER)
 		add_history(user_input);
